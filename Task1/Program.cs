ff51e8f7aff203bd21897c941ffdc30f4f92d61a
Кошельников Владимир <vkoshelnikov@bk.ru>
2023-09-05 18:26:22 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Метод, который соберет всех учеников всех классов в один список, используя LINQ.
+        /// От себя добавил еще сортировку
+        /// </summary>
+        /// <param name="classes"></param>
+        /// <returns></returns>
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return (classes.SelectMany(x => x.Students).OrderBy(s=>s).ToArray());
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
